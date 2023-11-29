4c1bb1a87f988f94289d40b5d24df47f33cda5ef
GaneshAnde <abnpganesh@gmail.com>
2023-11-29 22:07:54 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import React, { Component} from 'react'
 function App() {
   return (
     <div>
+    <div className='header'>
      Phone Directory
+    </div>
+    <button>Add</button>
+    <div>
+      <span>Name</span><br></br>
+      <span>Phone</span>
+
+    </div>
     </div>
   );
 }
