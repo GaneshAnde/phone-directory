a515cb1fd0f1befd652f8edee81eac7e15aeb107
GaneshAnde <abnpganesh@gmail.com>
2023-12-06 23:42:53 +0530

    Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React, { Component} from 'react'
+import React, { Component } from 'react'
 import Header from './Header';
+import './App.css'
 
-function App() {
-  return (
-    // <div>
-    // <h1>UpGrad</h1>
-    // <p>We are your F1 button for education!</p>,
-    // document.getElementById("root")
-    // </div>
-    <div>
-    {/* <div className='header'>
-     Phone Directory
-    </div> */}
-    <Header/>
-    <button>Add</button>
-    <div>
-      <span>Name</span><br></br>
-      <span>Phone</span>
+class App extends Component{
 
-    </div>
-    </div>
-  );
-}
+  render(){
+    var subscribers = [
+      {
+        id : 1,
+        name: 'Srithi',
+        phone:'9989999999'
+      },
+      {
+        id : 2,
+        name: 'Srithi',
+        phone:'00000000'
+      },
+    ]
+    return (
+
+      
+      // <div>
+      // <h1>UpGrad</h1>
+      // <p>We are your F1 button for education!</p>,
+      // document.getElementById("root")
+      // </div>
+      <div>
+        {/* <div className='header'>
+       Phone Directory
+      </div> */}
+        <Header />
+        <div className="component-body-container">
+            <button className="custom-btn add-btn">Add</button>
+  
+            <div className="grid-container heading-container">
+              <span className="grid-item name-heading">Name</span>
+              <span className="grid-item phone-heading">Phone</span>
+            </div>          
+
+            {
+              subscribers.map(sub =>{
+                return  <div key = {sub.id} className="grid-container">
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+              </div>
+              })
+            }
+        </div>
+  
+      </div>
+    );
+  }
+  
+};
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,14 @@ import './Header.css'
 
 const Header = function(){
     //style={{textAlign:"center",background:"#000", color:'#fff', padding:20, fontSize:20,textTransform:'uppercase' }}
+    
     return (
-        <div className="header">
-            Phone Directory
+        <div>
+            <div className="header">
+                Phone Directory
+            </div>
         </div>
+        
     )
 }
 
